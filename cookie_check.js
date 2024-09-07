0bc5c8dd5cc8f964e413005980ed2238f39434fd
MaxMiL4N <maksnoga123@gmail.com>
2024-09-07 15:22:36 +0200

    drff

---
+++ cookie_check.js
@@ -0,0 +1,4 @@
+if(document.cookie = "warning_turned_off=1"){
+    var x = document.getElementById("div_warning");
+    x.style.display = "none";
+}
---
+++ warning_off.js
@@ -0,0 +1,9 @@
+function myFunction2() {
+    document.cookie = "warning_turned_off=1";
+    var x = document.getElementById("div_warning");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
