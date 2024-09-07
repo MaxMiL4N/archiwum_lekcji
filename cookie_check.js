f12ee2d1b6ff818cc9eb84d95e8cff22530fdac9
MaxMiL4N <maksnoga123@gmail.com>
2024-09-07 15:25:30 +0200

    vvv
    vvvv

--- cookie_check.js
+++ cookie_check.js
@@ -1,5 +1,5 @@
+var x = document.getElementById("div_warning");
 if(document.cookie = "warning_turned_off=1"){
-    var x = document.getElementById("div_warning");
     x.style.display = "none";
 }
 else{
