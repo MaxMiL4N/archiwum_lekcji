75bc7872f6114539be5d9ab4e3ab06b43766b829
MaxMiL4N <maksnoga123@gmail.com>
2024-09-18 17:00:11 +0200

    fff

--- cookie_check.js
+++ cookie_check.js
@@ -9,16 +9,17 @@ function getCookie(cookieName) {
        let [name, value] = cookie.split("=");
       
        if (name === cookieName) {
- //         return decodeURIComponent(value);
-            return value;
+          return decodeURIComponent(value);
+ //           return value;
        }
     }
     
     return null;
  }
 console.log(getCookie("dsa"));
-if(getCookie("dsa")=="true"){
+if(getCookie("dsa")==true){
     x.style.display = "none";
+    console.log("brr")
 }
 else{
     x.style.display = "block";
