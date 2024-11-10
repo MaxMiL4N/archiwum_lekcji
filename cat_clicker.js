6dd2aaf1d507fa398b96fe793c861c9f9c81d4fc
MaxMiL4N <maksnoga123@gmail.com>
2024-11-10 23:46:57 +0100

    kicia counter TESTS 2

--- cat_clicker.js
+++ cat_clicker.js
@@ -8,21 +8,4 @@ function klikniecie_kici() {
     kici_counter.innerHTML="<img src='cat_cursor.png' height='25px'>"+counter_kicia;
 
     document.cookie="klikniecia_kici="+counter_kicia;
-
-
-    
 }    
-function KiciaClicksfunction() {
-
-
-
-      /*  document.cookie = "fff=true";
-        console.log("Warning hidden successfully.");
-        var x = document.getElementById("div_warning");
-        if (x.style.display == "none") {
-          x.style.display = "block";
-        } else {
-          x.style.display = "none";
-        }
-          */
-      }
