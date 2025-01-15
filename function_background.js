35fc37dfa6b3c3258b837874c5d5fee0a4567f0b
MaxMiL4N <maksnoga123@gmail.com>
2025-01-16 00:44:06 +0100

    wow

--- function_background.js
+++ function_background.js
@@ -5,7 +5,7 @@ if (background_checkbox.checked==true){
     document.getElementById("label_checkbox_center").style.backgroundColor = 'rgba(37, 26, 189, 0.4)';
 }
 else {
-    document.getElementById("javascript").style.backgroundImage = 'url("no_bg.png")';
+    document.getElementById("javascript").style.backgroundImage = 'unset';
     document.getElementById("javascript").style.transition = '0.5s';
     document.getElementById("label_checkbox_center").style.color = '';
     document.getElementById("label_checkbox_center").style.backgroundColor = '';
@@ -19,7 +19,7 @@ function change_background() {
         document.getElementById("label_checkbox_center").style.backgroundColor = 'rgba(37, 26, 189, 0.4)';
     }
     else {
-        document.getElementById("javascript").style.backgroundImage = 'url("no_bg.png")';
+        document.getElementById("javascript").style.backgroundImage = 'unset';
         document.getElementById("javascript").style.transition = '0.5s';
         document.getElementById("label_checkbox_center").style.color = 'lightgray';
         document.getElementById("label_checkbox_center").style.backgroundColor = 'rgba(89, 92, 99, 0.4)';
